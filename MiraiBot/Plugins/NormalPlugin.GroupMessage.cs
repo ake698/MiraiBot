c003bd05c7b1bc2d0396654ec77bfa1aa357e0ec
hunk <hunk5431@gmail.com>
2021-01-24 15:33:43 +0800

    添加图片撤回

--- MiraiBot/Plugins/NormalPlugin.GroupMessage.cs
+++ MiraiBot/Plugins/NormalPlugin.GroupMessage.cs
@@ -19,9 +19,16 @@ namespace MiraiBot.Plugins
 
         public async Task<bool> GroupMessage(MiraiHttpSession session, IGroupMessageEventArgs e) // 法2: 使用 params IMessageBase[]
         {
-            var replyMsg = await GroupMessageHandler(e);
+            if (e.Chain[1].Type == "Image")
+            {
+                var source = (SourceMessage)e.Chain[0];
+                await session.RevokeMessageAsync(source.Id);
+                return true;
+        }
+        var replyMsg = await GroupMessageHandler(e);
             if (replyMsg == null || replyMsg.Message == string.Empty)
                 return true;
+
             await session.SendGroupMessageAsync(e.Sender.Group.Id, new AtMessage(e.Sender.Id), replyMsg/*, plain2, /* etc... */); // 向消息来源群异步发送由以上chain表示的消息
             return false; // 不阻断消息传递。如需阻断请返回true
         }
@@ -35,7 +42,7 @@ namespace MiraiBot.Plugins
 
             $"收到了来自{e.Sender.Name}[{e.Sender.Id}]{{{e.Sender.Permission}}}的群消息:{string.Join(null, (IEnumerable<IMessageBase>)e.Chain)}".LogInfo();
             //     / 发送者群名片 /  / 发送者QQ号 /   /   发送者在群内权限
-
+            
             return await _groupHandler.CommandHandler(e);
         }
     }
